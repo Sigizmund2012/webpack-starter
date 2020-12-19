9924532ac768ade7638ca74cf79bc1e2933cac8b
Sergey Segeda <segeda823@mail.ru>
2020-12-19 17:05:42 +0300

    Change css minimizer to css-minimizer-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,11 @@
 const TerserPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
 });
